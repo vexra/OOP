5fcdc5c04610ad383afd3a2f592e1a50ee0ffd36
vexra <unique123wowros@gmail.com>
2023-10-01 09:25:17 +0700

    make test.txt

---
+++ Test.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner inputUser = new Scanner(System.in);
+        
+        int n = inputUser.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = inputUser.nextInt();
+        }
+
+        int query = inputUser.nextInt();
+        boolean found = false;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == query) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) System.out.println("Nilai ditemukan");
+        else System.out.println("Nilai tidak ditemukan");
+
+
+        inputUser.close();
+
+    }
+}
